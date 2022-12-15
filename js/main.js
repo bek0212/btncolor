c5409a50a5eb02909242c88e9cde2a1e7ab1acb9
bek0212 <muxammadjonovbekzodbek74@gmail.com>
2022-12-15 16:45:17 +0300

    btnbgcolor

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function salom(){
+    document.body.style.backgroundColor = 'yellow'
+}
+function qizil(){
+    document.body.style.backgroundColor = 'red'
+}
+function yashil(){
+    document.body.style.backgroundColor = 'green'
+}
+function qora(){
+    document.body.style.backgroundColor = 'black'
+}
